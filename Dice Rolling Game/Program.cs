3fe8ac68dd502a6bc9ca4b2e336381d314c9f10a
Martin Lietz <martin.lietz@messer-br.com>
2022-03-18 18:42:38 -0300

    First Commit

---
+++ Dice Rolling Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dice_Rolling_Game.Model;
+
+
+namespace Dice_Rolling_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
